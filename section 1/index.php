fbca0dde2fd4ffbb00be739dd995114e1151de3b
HanifAjiPrasetyo <hanifaji007@gmail.com>
2023-03-26 16:35:33 +0700

    move files etc

--- section 1/index.php
+++ section 1/index.php
@@ -1,28 +1,69 @@
 <?php
-$books = [
-    [
-        'name' => "Do Androids Dream of Electric Sheep",
-        'author' => "Philip K. Dick",
-        'releaseYear' => 1968,
-        'purchaseUrl' => "http://example.com"
-    ],
-    [
-        'name' => "Project Hail Mary",
-        'author' => "Andy Weir",
-        'releaseYear' => 2021,
-        'purchaseUrl' => "http://example.com"
-    ],
-    [
-        'name' => "The Martian",
-        'author' => "Andy Weir",
-        'releaseYear' => 2011,
-        'purchaseUrl' => "http://example.com"
-    ]
-];
+// ASSOCIATIVE ARRAY
+// $books = [
+//     [
+//         'name' => "Do Androids Dream of Electric Sheep",
+//         'author' => "Philip K. Dick",
+//         'releaseYear' => 1968,
+//         'purchaseUrl' => "http://example.com"
+//     ],
+//     [
+//         'name' => "Project Hail Mary",
+//         'author' => "Andy Weir",
+//         'releaseYear' => 2021,
+//         'purchaseUrl' => "http://example.com"
+//     ],
+//     [
+//         'name' => "The Martian",
+//         'author' => "Andy Weir",
+//         'releaseYear' => 2011,
+//         'purchaseUrl' => "http://example.com"
+//     ]
+// ];
+
+// $books[3] = [
+//     'name' => "TEST",
+//     'author' => "Philip K. Dick",
+//     'releaseYear' => 1968,
+//     'purchaseUrl' => "http://example.com"
+// ];
+
+
+
+// function filterByAuthor($books, $author)
+// {
+//     $filteredBooks = [];
+
+//     foreach($books as $book){
+//         if($book['author'] === $author){
+//         }
+//     }
+// }
+
+// unset($books[2]);
+// foreach ($books as $book) {
+//     echo '<ul><li><a href="$book["purchaseUrl"]"><h4>' . $book["name"] . '(' . $book["releaseYear"] . ') - By ' . $book["author"] . '</h4></a></li></ul>';
+// }
 
 // Built-in function untuk filtering array
-$filteredBooks = array_filter($books, function ($book) {
-    return $book['releaseYear'] > 2000;
-});
+// $filteredBooks = array_filter($books, function ($book) {
+//     return $book['releaseYear'] > 2000;
+// });
 
-require 'index.view.php';
+// require 'index.view.php';
+
+// LAMBDA FUNCTION
+
+// EXTRACT VARIABLES
+// function greeting($name)
+// {
+//     return "Halo $name";
+// }
+// $greetings = greeting("Hanif");
+// echo $greetings;
+
+// // ANONYMOUS / LAMBDA FUNCTION
+// $greetings = function ($name) {
+//     return "Halo $name";
+// };
+// echo $greetings("Hanif");
---
+++ section 2/views/index.view.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+    <style>
+        .bg-link {
+            background-color: greenyellow;
+        }
+
+        nav ul li {
+            display: inline;
+        }
+    </style>
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li>
+                <a href="/" class="<?= $_SERVER['REQUEST_URI'] === '/' ? 'bg-link' : '' ?>">Home</a>
+            </li>
+            <li>
+                <a href="/about.php" class="<?= $_SERVER['REQUEST_URI'] === '/about.php' ? 'bg-link' : '' ?>">About</a>
+            </li>
+            <li>
+                <a href="/contact.php" class="<?= $_SERVER['REQUEST_URI'] === '/contact.php' ? 'bg-link' : '' ?>">Contact</a>
+            </li>
+        </ul>
+    </nav><br><br>
+    <h2>INI HALAMAN HOME</h2>
+</body>
+
+</html>
